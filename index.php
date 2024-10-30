b8e54f4f0d4cc82f90386846f4443c8b6ccc13be
victor <vrequenaga@uoc.edu>
2024-10-30 20:24:41 +0100

    subiendo archivos

---
+++ index.php
@@ -0,0 +1,40 @@
+  <!doctype html>
+  <html lang="en">
+
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <script src="node_modules/sweetalert2/dist/sweetalert2.min.js"></script>
+    <link rel="stylesheet" href="node_modules/sweetalert2/dist/sweetalert2.min.css">
+  </head>
+
+  <body>
+    <div class="container text-center">
+      <div class="row">
+        <div class="col">
+          <div class="card">
+            <div class="card-header">
+              MINION
+            </div>
+            <div class="card-body">
+              <h5 class="card-title">Special title treatment</h5>
+              <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+              <a href="https://google.es" class="btn btn-primary">Go somewhere</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <script>
+
+      Swal.fire("SweetAlert2 is working!");
+
+    </script>
+  </body>
+
+  </html>
