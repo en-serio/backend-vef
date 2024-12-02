9d6c9b92a60eb945d2d5e962e94d97691878cc4b
viktoriyo <vrequenaga@uoc.edu>
2024-12-02 22:17:16 +0100

    bugfixes

--- class/entity/dbConnection.php
+++ class/entity/dbConnection.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 <?php
 
 
@@ -24,4 +25,31 @@ class Database
         return $conn;
     }
 }
+=======
+<?php
+
+class Database
+{
+
+    private $host = 'localhost';
+    private $db_name = 'vef_php';
+    private $username = 'root';
+    private $password = 'AideaMariaDB..';
+    private $conn;
+
+    public function getConnection()
+    {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        } catch (PDOException $e) {
+            $e->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+>>>>>>> 57a8bd8 (bugfixes)
 ?>
